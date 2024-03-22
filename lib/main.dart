6a55fc1a3a0a8c3ccd269fbbb1b3b0a6abaebeb1
StilOn43 <adamfedorowiczSN43@gmail.com>
2024-03-22 06:04:13 +0100

    Implementing basic versions of login and registration.

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,77 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: 'Navigation Basics',
-      home: const FirstRoute(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false));
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).whenComplete(() {
+    runApp(MaterialApp(
+        title: 'Navigation Basics',
+        home: const FirstRoute(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false));
+  });
 }
 
-class FirstRoute extends StatelessWidget {
+// class ToastUtils {
+//   static void showToast(String messageKey, BuildContext context) {
+//     Fluttertoast.showToast(
+//       msg: AppLocaleUtils.of(context).translate(messageKey),
+//       toastLength: Toast.LENGTH_SHORT,
+//       gravity: ToastGravity.CENTER,
+//       backgroundColor: Colors.black,
+//       textColor: Colors.white,
+//       fontSize: Dimens.fontSizeStandard,
+//     );
+//   }
+// }
+
+final emailController = TextEditingController();
+final passwordController = TextEditingController();
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class FirstRoute extends StatefulWidget {
   const FirstRoute({super.key});
+
+  @override
+  State<FirstRoute> createState() => _FirstRouteState();
+}
+
+class _FirstRouteState extends State<FirstRoute> {
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in! Navigate to main page');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const ThirdRoute()),
+        );
+      }
+    });
+  }
+
+  String message = ' ';
+
+  void _login() async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      setState(() {
+        message = '';
+      });
+    } catch (error) {
+      setState(() {
+        message = error.toString();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +98,9 @@ class FirstRoute extends StatelessWidget {
                   }
                   return null;
                 },
+                controller: emailController,
                 decoration: InputDecoration(
-                  hintText: 'Login',
+                  hintText: 'E-mail',
                   border: OutlineInputBorder(
                     borderSide: BorderSide(width: 2.0),
                   ),
@@ -55,6 +116,7 @@ class FirstRoute extends StatelessWidget {
                   }
                   return null;
                 },
+                controller: passwordController,
                 decoration: InputDecoration(
                   hintText: 'Password',
                   border: OutlineInputBorder(
@@ -70,11 +132,15 @@ class FirstRoute extends StatelessWidget {
                 height: 30, // <-- Your height
                 child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SecondRoute()),
-                    );
+                    _login();
+                    if (message == '') {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ThirdRoute()),
+                      );
+                    }
+                    ;
                   },
                   child: Text('Login', style: TextStyle(fontSize: 16)),
                 ),
@@ -84,10 +150,9 @@ class FirstRoute extends StatelessWidget {
               text: TextSpan(
                   text: "I don't have an account",
                   style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 14,
-                      fontWeight: FontWeight.bold
-                  ),
+                      color: Colors.deepPurple,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold),
                   recognizer: TapGestureRecognizer()
                     ..onTap = () {
                       Navigator.push(
@@ -97,6 +162,11 @@ class FirstRoute extends StatelessWidget {
                       );
                     }),
             ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text(message,
+                  style: TextStyle(color: Colors.red, fontSize: 12)),
+            ),
           ],
         ),
       ),
@@ -104,8 +174,32 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
-class SecondRoute extends StatelessWidget {
+class SecondRoute extends StatefulWidget {
   const SecondRoute({super.key});
+
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  String message = ' ';
+
+  void _register() async {
+    setState(() {
+      message = ' ';
+    });
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      setState(() {
+        message = '';
+      });
+    } catch (error) {
+      setState(() {
+        message = error.toString();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -126,23 +220,7 @@ class SecondRoute extends StatelessWidget {
                   }
                   return null;
                 },
-                decoration: InputDecoration(
-                  hintText: 'Login',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(width: 2.0),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30, 2, 30, 2),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter proper text';
-                  }
-                  return null;
-                },
+                controller: emailController,
                 decoration: InputDecoration(
                   hintText: 'E-mail',
                   border: OutlineInputBorder(
@@ -160,6 +238,7 @@ class SecondRoute extends StatelessWidget {
                   }
                   return null;
                 },
+                controller: passwordController,
                 decoration: InputDecoration(
                   hintText: 'Password',
                   border: OutlineInputBorder(
@@ -174,14 +253,46 @@ class SecondRoute extends StatelessWidget {
                 width: 300, // <-- Your width
                 height: 30, // <-- Your height
                 child: ElevatedButton(
+                  //onPressed: () {Navigator.pop(context);},
                   onPressed: () {
-                    Navigator.pop(context);
+                    _register();
+                    if (message == '') {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ThirdRoute()),
+                      );
+                    }
+                    ;
                   },
                   child: Text('Register', style: TextStyle(fontSize: 16)),
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text(message,
+                  style: TextStyle(color: Colors.red, fontSize: 12)),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map Page'),
+      ),
+      body: Center(
+        child: Column(
+          children: [const Text('Map page')],
         ),
       ),
     );
